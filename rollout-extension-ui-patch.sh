901d2d00042b01ac935507c5650d155efcba8b64
root <root@jake1.local1.site>
2025-08-16 16:31:08 +0200

    updates 16-08-2025

--- rollout-extension-ui-patch.sh
+++ rollout-extension-ui-patch.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
 o patch -n openshift-gitops argocd openshift-gitops -p "$(cat rollout-extension-ui-patch.yaml)" --type merge
 o -n openshift-gitops rollout restart deployment argo-rollouts cluster gitops-plugin openshift-gitops-applicationset-controller openshift-gitops-dex-server openshift-gitops-redis openshift-gitops-server openshift-gitops-repo-server
 o -n openshift-gitops rollout restart statefulset openshift-gitops-application-controller
+
